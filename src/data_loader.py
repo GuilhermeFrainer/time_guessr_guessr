e4c73b9c11727d7c9a1236405d93876eb8f47e24
Guilherme Frainer <guilherme.sfrainer@gmail.com>
2024-12-29 15:03:30 -0300

    Updated DataLoader class to work with larger datasets properly. Added scaling.

--- src/data_loader.py
+++ src/data_loader.py
@@ -1,12 +1,142 @@
 import polars as pl
-import os
 import pathlib
 import numpy as np
 from tensorflow.keras.utils import img_to_array, load_img
 from sklearn.preprocessing import MinMaxScaler
+import tensorflow as tf
+import filetype
+from scaler import Scaler
 
 
-def load_all_data(dir: pathlib.Path, target_size=(224, 224), scaling=True, start_year=1900, end_year=2025) -> tuple[np.ndarray, np.ndarray]:
+class DataLoader:
+    data_path: pathlib.Path
+    df: pl.DataFrame
+
+
+    def __init__(self, data_path: str):
+        self.data_path = pathlib.Path(data_path)
+        self.df = pl.read_csv(self.data_path / "metadata.csv")
+
+
+    def dataset(self, target_size=(224, 224), batch_size=32, scaling=True) -> tf.data.Dataset:
+        """
+        Gets TensorFlow Dataset to train model.
+        This loads the data in batches, allowing for a large dataset
+        (i.e. one that does not fit in memory) to be used.
+
+        Args:
+            target_size : tuple[int, int],  default=(224,224)
+                Size to resize images to (height, width).
+
+            batch_size : int, default=32
+                Size of the batch of training data.
+
+            scaling : bool, default=True
+                Whether or not to scale coordinates.
+                Uses min-max scaling.
+        Returns:
+            tf.data.Dataset:
+                Dataset to be used in training.
+        """
+        # Clean-up dataset
+        self.df = self.__filter_out_gifs(self.df)
+        self.df = self.__filter_out_non_existent_files(self.df)
+
+        paths: pl.Series = str(self.data_path) + "/" + self.df["year"].cast(pl.String) 
+        paths += "/" + self.df["id"].cast(pl.String) + ".jpg"
+        paths = paths.to_numpy()
+        
+        if scaling:
+            years = Scaler.scale_years(self.df["year"].to_numpy())
+            lats = Scaler.scale_latitudes(self.df["latitude"].to_numpy())
+            lons = Scaler.scale_longitudes(self.df["longitude"].to_numpy())
+        else:
+            years = self.df["year"].to_numpy()
+            lats = self.df["latitude"].to_numpy()
+            lons = self.df["longitude"].to_numpy()
+
+        dataset = tf.data.Dataset.from_tensor_slices((paths, years, lats, lons))
+        dataset = dataset.map(
+            lambda path, year, lat, lon: DataLoader.load_image_and_labels(path, year, lat, lon, image_shape=target_size)
+        )
+
+        dataset = dataset.shuffle(buffer_size=len(paths)).batch(batch_size).prefetch(tf.data.AUTOTUNE)
+        return dataset
+
+
+    def consolidate_metadata(self):
+        metadata_df = self.__load_all_metadata(self.data_path)
+        metadata_df.write_csv(self.data_path / "metadata.csv")
+
+
+    @staticmethod
+    def load_image_and_labels(path: str, year: int, lat: float, lon: float, image_shape=(224, 224)):
+        img = tf.io.read_file(path)
+        img = tf.image.decode_jpeg(img, channels=3)
+        img = tf.image.resize(img, image_shape) / 255.0
+        return img, {
+            "year": year,
+            "lat": lat,
+            "lon": lon
+        }
+
+
+    def __filter_out_gifs(self, df: pl.DataFrame) -> pl.DataFrame:
+        def is_gif(filepath: str) -> bool:
+            guess = filetype.guess(filepath)
+            return guess.mime == "image/gif"
+        
+        original_columns = df.columns
+        df = df.with_columns(
+            (
+                str(self.data_path) + "/" + pl.col("year").cast(pl.String) + "/" + pl.col("id").cast(pl.String) + ".jpg"
+            ).alias("filepath")
+        )
+        df = df.with_columns(
+            (
+                df["filepath"].map_elements(is_gif, return_dtype=pl.Boolean)
+            ).alias("is_gif")
+        )
+        df = df.filter(~pl.col("is_gif"))
+        return df.select(original_columns)
+
+
+    def __filter_out_non_existent_files(self, df: pl.DataFrame) -> pl.DataFrame:
+        def file_exists(filepath: str) -> bool:
+            file = pathlib.Path(filepath)
+            return file.is_file()
+        
+        original_columns = df.columns
+        df = df.with_columns(
+            (
+                str(self.data_path) + "/" + pl.col("year").cast(pl.String) + "/" + pl.col("id").cast(pl.String) + ".jpg"
+            ).alias("filepath")
+        )
+        df = df.with_columns(
+            (
+                df["filepath"].map_elements(file_exists, return_dtype=pl.Boolean)
+            ).alias("file_exists")
+        )
+        df = df.filter(pl.col("file_exists"))
+        return df.select(original_columns)
+
+
+    def __load_all_metadata(self, images_dir: pathlib.Path) -> pl.DataFrame:
+        """
+        Loads all metadata files into a single DataFrame.
+        """
+        csvs = []
+        for d in images_dir.iterdir():
+            if d.is_dir():
+                df = pl.read_csv(d / "metadata.csv")
+                csvs.append(df)
+        return pl.concat(csvs)
+
+
+def load_all_data(dir: pathlib.Path, target_size=(224, 224),
+    scaling=True, scaler=MinMaxScaler(),
+    start_year=1900, end_year=2025
+) -> tuple[np.ndarray, np.ndarray, ]:
     """
     Loads images and labels from a directory.
 
@@ -20,6 +150,9 @@ def load_all_data(dir: pathlib.Path, target_size=(224, 224), scaling=True, start
         scaling : bool, default=True
             Whether or not to scale coordinates.
 
+        scaler : Optional[sklearn.StandardScaler], default=MinMaxScaler
+            The scaler chosen for scaling. Will be returned by the function to allow for easy descaling.
+
         start_year : int, default=1900
             First year of the range to include images from (inclusive).
 
@@ -28,11 +161,17 @@ def load_all_data(dir: pathlib.Path, target_size=(224, 224), scaling=True, start
 
     Returns:
         np.ndarray
-            array of preprocessed images
+            Array of preprocessed images
 
         np.ndarray
-            array of labels (year, latitude, longitude)
+            Array of labels (year, latitude, longitude)
+
+        scaler
+            The scaler used.
     """
+    if scaling and not scaler:
+        raise ValueError("A scaler must be provided if scaling was chosen.")
+
     if isinstance(dir, str):
         dir = pathlib.Path(dir)
 
@@ -50,10 +189,9 @@ def load_all_data(dir: pathlib.Path, target_size=(224, 224), scaling=True, start
     label_arr = np.concatenate(labels)
 
     if scaling:
-        scaler = MinMaxScaler()
         label_arr[:, 1:] = scaler.fit_transform(label_arr[:, 1:])
 
-    return image_arr, label_arr
+    return image_arr, label_arr, scaler
 
 
 def __load_data_for_year(dir: pathlib.Path, target_size=(224,224)) -> tuple[np.ndarray, np.ndarray]:
---
+++ src/scaler.py
@@ -0,0 +1,74 @@
+import numpy as np
+
+
+class Scaler:
+    YEAR_MIN = 1900
+    YEAR_MAX = 2024
+
+    LAT_MIN = -90
+    LAT_MAX = 90
+
+    LON_MIN = -180
+    LON_MAX = 180
+
+
+    @staticmethod
+    def scale_labels(labels: np.ndarray) -> np.ndarray:
+        """
+        Scales label data.
+
+        Args:
+            labels : np.ndarray
+                Labels to be scaled.
+                Should be an n-dimensional array of 3-dimensional arrays with [year, latitude, longitude].
+
+        Returns:
+            np.ndarray:
+                Scaled labels
+        """
+        out_labels = labels
+        out_labels[:, 0:1] = Scaler.scale_years(labels[:, 0:1])
+        out_labels[:, 1:2] = Scaler.scale_latitudes(labels[:, 1:2])
+        out_labels[:, 2:3] = Scaler.scale_longitudes(labels[:, 2:3])
+        return out_labels
+
+
+    @staticmethod
+    def scale_longitudes(lons: np.ndarray) -> np.ndarray:
+        return Scaler.__min_max_scaling(lons, min=Scaler.LON_MIN, max=Scaler.LON_MAX)
+
+
+    @staticmethod
+    def scale_latitudes(lats: np.ndarray) -> np.ndarray:
+        return Scaler.__min_max_scaling(lats, min=Scaler.LAT_MIN, max=Scaler.LAT_MAX)
+
+
+    @staticmethod
+    def scale_years(years: np.ndarray) -> np.ndarray:
+        return Scaler.__min_max_scaling(years, min=Scaler.YEAR_MIN, max=Scaler.YEAR_MAX)
+
+
+    @staticmethod
+    def __min_max_scaling(data: np.ndarray, min=0, max=1) -> np.ndarray:
+        """
+        Generic function to scale data in a known range to [0, 1].
+        Scaling is performed as such:
+
+        y_scaled = \\frac{y - y_min}{y_max - y_min}
+
+        Args:
+            data : np.ndarray
+                Data to be scaled.
+
+            min : int, default=0
+                Smallest element in data range.
+
+            max : int, default=1
+                Largest element in data range.
+
+        Returns:
+            np.ndarray:
+                Scaled data.
+        """
+        return (data - min) / (max - min)
+
